67755ecfb483c1587f075ce413e278927f862318
Thang <48205619+dauthang81197@users.noreply.github.com>
2026-01-27 23:23:15 +0700

    Update environment.ts

--- apps/api-gateway/env/environment.ts
+++ apps/api-gateway/env/environment.ts
@@ -11,12 +11,12 @@ export const ENVIRONMENT = {
   },
   redis: { host: '192.168.50.22', port: 6379 },
   r2: {
-    accountId: 'e751f9503f859179aea8642167ce5bfd',
-    accessKeyId: '8c518e8059ae8131ac69c738bccd1bd5',
+    accountId: '',
+    accessKeyId: '',
     secretAccessKey:
-      '6cc2017c09e33f276e1c0b01657ea8cb2fd829c474847a0bc5cdd2b27098dd89',
-    bucketName: 'course',
-    publicUrl: 'https://pub-5e52d815a0ea48b5aa905910e63faf7b.r2.dev',
+      '',
+    bucketName: '',
+    publicUrl: '',
   },
 };
 
@@ -30,11 +30,11 @@ export const ENVIRONMENT_DEV = {
   },
   redis: { host: '192.168.50.22', port: 6379 },
   r2: {
-    accountId: 'e751f9503f859179aea8642167ce5bfd',
-    accessKeyId: '8c518e8059ae8131ac69c738bccd1bd5',
+    accountId: '',
+    accessKeyId: '',
     secretAccessKey:
-      '6cc2017c09e33f276e1c0b01657ea8cb2fd829c474847a0bc5cdd2b27098dd89',
+      '',
     bucketName: 'course',
-    publicUrl: 'https://pub-5e52d815a0ea48b5aa905910e63faf7b.r2.dev',
+    publicUrl: '',
   },
 };
